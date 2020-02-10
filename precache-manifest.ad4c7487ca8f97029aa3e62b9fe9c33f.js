c7a05318b285ff848a46874a4c0dc40fda8f8dc9
Wanja Kuch <wanjakuch@gmail.com>
2020-02-10 01:38:45 +0100

    Updates

---
+++ precache-manifest.ad4c7487ca8f97029aa3e62b9fe9c33f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96214ca60cd478f9388bc51894e22f16",
+    "url": "/Build-a-Frizzel/index.html"
+  },
+  {
+    "revision": "ba56fd9670a3d99fd9fd",
+    "url": "/Build-a-Frizzel/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c762687669d1ad6ac27c",
+    "url": "/Build-a-Frizzel/static/js/2.c84b7ea8.chunk.js"
+  },
+  {
+    "revision": "b596809885292289c442c2a0c7f59d96",
+    "url": "/Build-a-Frizzel/static/js/2.c84b7ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba56fd9670a3d99fd9fd",
+    "url": "/Build-a-Frizzel/static/js/main.58cf3c47.chunk.js"
+  },
+  {
+    "revision": "171ed70a2f53c3811678",
+    "url": "/Build-a-Frizzel/static/js/runtime-main.c9e21bf2.js"
+  },
+  {
+    "revision": "da2235476c2418da7ebbdcc3c67ca987",
+    "url": "/Build-a-Frizzel/static/media/fill.da223547.png"
+  },
+  {
+    "revision": "0abe64353dcc73f60cd800fa222a08c7",
+    "url": "/Build-a-Frizzel/static/media/fill_2.0abe6435.png"
+  },
+  {
+    "revision": "ce29c02aa302c73f4378e5a0923ba500",
+    "url": "/Build-a-Frizzel/static/media/siluette.ce29c02a.png"
+  }
+]);
